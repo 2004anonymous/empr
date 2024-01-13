58df211cc76131897cd86cf74c31421b2a405ffb
anonymous <108885302+2004anonymous@users.noreply.github.com>
2024-01-13 22:33:15 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Url = "mysql+pymysql://root@localhost:3306/test"
+# Url = "mysql+pymysql://root@localhost:3306/test"
+Url = "postgresql://anonymous:V7p0qCcLybCGWED8VZVECAj6oJoAhHQL@dpg-cmd4ug821fec73d0a3mg-a.oregon-postgres.render.com/admin_db_ckfr"
 
 engine = create_engine(url=Url)
 
